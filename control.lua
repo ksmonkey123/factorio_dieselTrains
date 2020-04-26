137f5ba766389e944b2086c58b30f77cecd202f3
Andreas Wälchli <andreas.waelchli@me.com>
2020-04-26 15:33:40 +0200

    added support for KS power diesel

--- control.lua
+++ control.lua
@@ -2,6 +2,9 @@ local function ON_INIT()
 	remote.call("fluidTrains_hook", "addLocomotive", "Diesel-Locomotive-fluid-locomotive", 1500)
 	remote.call("fluidTrains_hook", "addFluid", "Diesel-Locomotive-fluid", "light-oil", {{item = "Diesel-Locomotive-light-oil"}})
 	remote.call("fluidTrains_hook", "addFluid", "Diesel-Locomotive-fluid", "heavy-oil", {{item = "Diesel-Locomotive-heavy-oil"}})
+	if game.fluid_prototypes["diesel-fuel"] then
+		remote.call("fluidTrains_hook", "addFluid", "Diesel-Locomotive-fluid", "diesel-fuel", {{item = "Diesel-Locomotive-diesel-fuel", temp = 0}})
+	end
 	
 	for _, force in pairs(game.forces) do
 	  if force.technologies["diesel-locomotive"].researched then
