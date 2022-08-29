e5d93e1118213f59efea9e889a37a7a634521460
main() <main@ehvag.de>
2022-08-29 23:37:44 +0200

    Implement compatibility with trainConstructionSite
    Trains need to be pre-filled with a light oil barrel because trainConstructionSite can't handle fluids

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -9,3 +9,9 @@ fluid_locomotive.color = {r = 1, g = 0.73, b = 0.07, a = 0.5}
 
 -- Add new locomotive to the game
 data:extend({fluid_locomotive})
+
+
+if mods["trainConstructionSite"] then
+  require("__trainConstructionSite__/modding-interface")
+  trainConstructionSite.remote.addCustomFuelTrain("locomotive", "Diesel-Locomotive-fluid-locomotive", "light-oil-barrel")
+end
